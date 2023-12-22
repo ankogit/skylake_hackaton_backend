8657b9ec4c8a788130ed7bcbde9b2ad72662d9eb
Anatoly Antonenko <ya.tolik-a@yandex.ru>
2023-12-23 01:46:21 +0300

    fix: https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Contracts\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,8 +18,11 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(UrlGenerator $url): void
     {
-        //
+        if ($this->app->environment('production')) {
+            $this->app['request']->server->set('HTTPS', 'on');
+            $url->forceScheme('https');
+        }
     }
 }
