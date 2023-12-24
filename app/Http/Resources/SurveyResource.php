1c2e8b3e7b0e6466e09207d1dc76887ee5c05d11
Anatoly Antonenko <ya.tolik-a@yandex.ru>
2023-12-24 13:27:09 +0300

    fix

--- app/Http/Resources/SurveyResource.php
+++ app/Http/Resources/SurveyResource.php
@@ -15,6 +15,7 @@ class SurveyResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'category' => new SurveyCategoryResource($this->category),
             'question' => $this->question,
         ];
